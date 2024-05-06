d7e1fec21f355d8590b6c2671b77a22b1be7ccdd
Alykhaled11 <khaledhod111@gmail.com>
2024-05-06 05:21:41 +0300

    new code

---
+++ weapon.py
@@ -0,0 +1,12 @@
+class Weapon:
+    def __init__(self, name: str, weapon_type: str, damage: int, value: int):
+        self.name = name
+        self.weapon_type = weapon_type
+        self.original_damage = damage
+        self.damage = damage
+        self.value = value
+
+
+sword = Weapon(name='Sword', weapon_type='close_range', damage=15, value=10)
+fists = Weapon(name='Fists', weapon_type='close_range', damage=5, value=0)
+bow = Weapon(name='Bow', weapon_type='long_range', damage=10, value=7)
